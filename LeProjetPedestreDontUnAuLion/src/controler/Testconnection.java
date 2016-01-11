68b438291a81a6cce25db5d4a04bc58d9eef286c
leovidal <leovidal@192.168.1.30>
2016-01-11 11:10:39 +0100

    Ajout d'un controleur et divers classe pour acceder à la base et gérer les exception SQL

---
+++ LeProjetPedestreDontUnAuLion/src/controler/Testconnection.java
@@ -0,0 +1,10 @@
+package controler;
+
+import java.sql.SQLException;
+
+public class Testconnection {
+
+	public static void main( String args[] ) throws SQLException {
+		Controleur controleur = new Controleur();
+	 }
+}
