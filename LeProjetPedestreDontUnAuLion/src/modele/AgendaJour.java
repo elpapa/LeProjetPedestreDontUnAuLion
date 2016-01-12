b775c762661e8024e5285f26703c23930138d9b4
leovidal <leovidal@192.168.1.30>
2016-01-12 14:36:52 +0100

    Ajout des classes Agenda

---
+++ LeProjetPedestreDontUnAuLion/src/modele/AgendaJour.java
@@ -0,0 +1,10 @@
+package modele;
+
+public class AgendaJour extends Agenda {
+
+	public AgendaJour(int idAlbum, Client proprietaire) {
+		super(idAlbum, proprietaire);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ LeProjetPedestreDontUnAuLion/src/modele/AgendaSemaine.java
@@ -0,0 +1,10 @@
+package modele;
+
+public class AgendaSemaine extends Agenda {
+
+	public AgendaSemaine(int idAlbum, Client proprietaire) {
+		super(idAlbum, proprietaire);
+		// TODO Auto-generated constructor stub
+	}
+
+}
