234b6c5cdca2ec8cd90dec8da1237c14d7f8872d
leovidal <leovidal@192.168.1.30>
2016-01-29 10:45:22 +0100

    test

--- LeProjetPedestreDontUnAuLion/src/controler/Accueil.java
+++ LeProjetPedestreDontUnAuLion/src/controler/Accueil.java
@@ -19,30 +19,41 @@ public class Accueil {
 		
 		System.out.println("Bienvenue Sur l'application des petits pedestres !!");
 
-			menu.menuApp();
+			//menu.menuApp();
 
-		/*RequeteClient rc = new RequeteClient(controleur.getConn());
-		Client c = new Client("johndiliger@gmail.com", "diliger", "john", "12 rue de monc", "ahah", false);
-		//rc.addClient(c);
+		//test client
+		RequeteClient rc = new RequeteClient(controleur.getConn());
+		Client c = new Client("boristhouvenin", "thouvenin", "boris", "marcel porte", "secret", false);
+		rc.addClient(c);
+		Client c2 = rc.getClientByMail("boristhouvenin");
+		System.out.println(c2.getNom());
 		
-		RequeteFichierImage rfi = new RequeteFichierImage(controleur.getConn());
-		FichierImage fi2 = rfi.getFichierImageFromId(3);
 		
-		Photo p = new Photo(0, "plage", "beach", fi2);
+		//test fichier image
+		/*RequeteFichierImage rfi = new RequeteFichierImage(controleur.getConn());
+		FichierImage fi = new FichierImage(0, "/", 12, false, 4000, 200, 4, c2);
+		FichierImage fi2 = rfi.getFichierImageFromId(1);
+		System.out.println(fi2.getPath());*/
+		
+		
+		//test photo
+		/*Photo p = new Photo(0, "plage", "beach", fi2);
 		RequetePhoto rp = new RequetePhoto(controleur.getConn());
-		Photo ski = new Photo(0, "ski", "neige", fi2);
+		rp.addPhoto(p);
+		Photo p2 = rp.getPhotoFromId(1);
+		System.out.println(p2.getTitre());*/
 		
-		Album album = new Album(0, c, null);
+		
+		//test album
+		/*Album album = new Album(0, c, "Vacances", null);
 		RequeteAlbum ra = new RequeteAlbum(controleur.getConn());
-		//ra.addAlbum(album);
-		Album zob = ra.getAllAlbumFromClient(c).get(0);
-		
-		Date date = new Date(2016, 01, 27);
-		Commande cmd = new Commande(0, date, 250, c, "envoye", false, new ArrayList<AlbumCommande>());
-		RequeteCommande rcmd = new RequeteCommande(controleur.getConn());
-		rcmd.addCommande(cmd);*/
+		ra.addAlbum(album);
+		Album album2 = ra.getAllAlbumFromClient(c).get(0);
+		System.out.println(album2.getNom());*/
 		
 		
+		//test ajout photo à un album
+		//ra.addPhotoToAlbum(album2, p);
 		
 
 	}
